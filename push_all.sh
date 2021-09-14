a6b1ce06f1bb416cfd80c417c289ba5e1f2a669a
toto <antonio.bonaduce.mo@gmail.com>
2021-09-14 12:21:57 +0200

    First commit

--- push_all.sh
+++ push_all.sh
@@ -30,7 +30,7 @@ git add .
 # Adds the files in the local repository and stages them for commit. To unstage a file, use 'git reset HEAD YOUR-FILE'.
 git commit -m "First commit"
 # Commits the tracked changes and prepares them to be pushed to a remote repository. To remove this commit and modify the file, use 'git reset --soft HEAD~1' and commit and add the file again.
-git remote add origin https://github.com/abonaduce/SEAPR.git
+git remote add origin https://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@github.com/abonaduce/SEAPR.git
 # Sets the new remote
 git remote -v
 # Verifies the new remote URL
